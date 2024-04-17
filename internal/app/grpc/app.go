e45df98a27a18db76dbc7f8ada103ca938badfac
WtoJglg4 <glazovv81@gmail.com>
2024-04-17 17:49:59 +0300

    feat: add app structs and Run and Stop methods

---
+++ internal/app/grpc/app.go
@@ -0,0 +1,72 @@
+package grpcapp
+
+import (
+	"fmt"
+	"github/sso-grpc/sso/internal/grpc/authgrpc"
+	"log/slog"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+type App struct {
+	log        *slog.Logger
+	gRPCServer *grpc.Server
+	port       int
+}
+
+// New creates new gRPC server app.
+func New(
+	log *slog.Logger,
+	port int,
+) *App {
+	// TODO: init storage
+	// TODO: init auth service layer
+
+	gRPCServer := grpc.NewServer()
+
+	authgrpc.Register(gRPCServer)
+
+	return &App{
+		log:        log,
+		gRPCServer: gRPCServer,
+		port:       port,
+	}
+}
+
+func (a *App) MustRun() {
+	if err := a.Run(); err != nil {
+		panic(err)
+	}
+}
+
+func (a *App) Run() error {
+	const op = "grpcapp.Run"
+
+	log := a.log.With(
+		slog.String("op", op),
+		slog.Int("port", a.port),
+	)
+
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", a.port))
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	log.Info("gRPC server is running", slog.String("addr", l.Addr().String()))
+
+	if err := a.gRPCServer.Serve(l); err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	return nil
+}
+
+func (a *App) Stop() {
+	const op = "grpcapp.Stopr"
+
+	a.log.With(slog.String("op", op)).
+		Info("stopping gRPC server", slog.Int("port", a.port))
+
+	a.gRPCServer.GracefulStop()
+}
