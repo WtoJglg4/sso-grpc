e63e9e9300bc6b3bffc558d1dc659193c61ff2b0
WtoJglg4 <glazovv81@gmail.com>
2024-04-19 23:53:58 +0300

    feat: add service layer

--- go.mod
+++ go.mod
@@ -10,8 +10,10 @@ require (
 
 require (
 	github.com/BurntSushi/toml v1.3.2 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/kr/pretty v0.3.1 // indirect
+	golang.org/x/crypto v0.22.0 // indirect
 	golang.org/x/net v0.24.0 // indirect
 	golang.org/x/sys v0.19.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
---
+++ internal/lib/jwt/jwt.go
@@ -0,0 +1,24 @@
+package jwt
+
+import (
+	"github/sso-grpc/sso/internal/domain/models"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+)
+
+func NewToken(user models.User, app models.App, duration time.Duration) (string, error) {
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	claims := token.Claims.(jwt.MapClaims)
+	claims["uid"] = user.ID
+	claims["email"] = user.Email
+	claims["app_id"] = app.ID
+	claims["exp"] = time.Now().Add(duration).Unix()
+
+	tokenString, err := token.SignedString([]byte(app.Secret))
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
+}
